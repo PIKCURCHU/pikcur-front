0c16917e7725bd0887360eae4ddb5f49a6312d4c
JoungEunYu <wjddmsdb124@gmail.com>
2025-10-09 00:23:55 +0900

    feat: add common component (CustomAvatar, CustomTable, InfoList) / add fontawesome(icon) / set notosans global font

---
+++ src/components/common/CustomTable.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+
+interface CustomTableProps {
+    width: number;
+    columns: ColumnDef[];
+    dataList: any[];
+    onRowClick?: (row: any) => void;
+}
+
+/** 공통 Table 컴포넌트
+ * 
+ * @example
+ * <CustomTable
+ *   width={600}
+ *   columns={[
+ *     { field: "name", headerName: "이름" },
+ *     { field: "age", headerName: "나이" },
+ *     { field: "city", headerName: "도시", render: (value: any) => <b>{value}</b> }, // render 함수로 커스텀 렌더링 가능, 이미지도 가능
+ *   ]}
+ *   dataList={[
+ *     { name: "홍길동", age: 28, city: "서울" },
+ *     { name: "김철수", age: 33, city: "부산" },
+ *   ]}
+ *   onRowClick={(row) => console.log("클릭한 행:", row)}
+ * />
+ *
+ * @param width 가로 크기
+ * @param columns 컬럼 정의
+ * @param dataList 데이터 리스트
+ * @param onRowClick 행 클릭 이벤트 핸들러
+ * @returns 
+ */
+const CustomTable: React.FC<CustomTableProps> = ({
+    width,
+    columns,
+    dataList,
+    onRowClick,
+}) => {
+    return (
+        <TableContainer component={Paper} sx={{ borderRadius: 2, width: width }}>
+            <Table sx={{ tableLayout: 'fixed', width: '100%' }}>
+                <TableHead>
+                    <TableRow>
+                        {columns.map((col) => (
+                            <TableCell
+                                key={col.field}
+                                align={col.align || "left"}
+                                sx={{
+                                    fontWeight: 500,
+                                    color: '#141414',
+                                    fontSize: 14,
+                                    whiteSpace: 'nowrap',
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                }}>
+                                {col.headerName}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    {dataList.map((row) => (
+                        <TableRow
+                            hover
+                            onClick={() => onRowClick?.(row)}
+                            sx={{ cursor: onRowClick ? "pointer" : "default" }}
+                        >
+                            {columns.map((col) => (
+                                <TableCell 
+                                key={col.field} 
+                                align={col.align || "left"} 
+                                sx={{
+                                    fontweight: 400, 
+                                    color: '#757575', 
+                                    fontSize: 14, 
+                                    whiteSpace: 'nowrap',
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                }}>
+                                    {col.render ? col.render(row[col.field], row) : row[col.field]}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default CustomTable;
---
+++ src/components/common/InfoList.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+interface InfoListProps {
+    data: Record<string, string | number>;
+    labelMap: Record<string, string>;
+}
+
+/** 공통 Table 컴포넌트
+ * @example
+ * <InfoList
+ *      data={{
+ *          name: '홍길동',
+ *          age: 30,
+ *          address: '서울시'
+ *      }}
+ *      labelMap={{ name: '이름', age: '나이' }}
+ * />
+ *
+ * @param data 컴포넌트에 들어갈 데이터
+ * @param labelMap 데이터의 제목을 매핑
+ * @returns 
+ */
+const InfoList: React.FC<InfoListProps> = ({ data, labelMap }) => {
+    const entries = Object.keys(labelMap)
+        .filter((key) => key in data)
+        .map((key) => ({ key, label: labelMap[key], value: data[key] }));
+
+    return (
+        <div style={{ display: 'flex', gap: '24px', fontSize: 14 }}>
+            {entries.map((item, index) => (
+                <div
+                    key={item.key}
+                    style={{
+                        borderTop: '1px solid #E5E8EB',
+                        width: index === 0 ? '186px' : '718px',
+                        height: '83px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        gap: 2
+                    }}
+                >
+                    <div style={{ color: '#61758A' }}>{item.label}</div>
+                    <div style={{ color: '#141414' }}>{item.value}</div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default InfoList;
